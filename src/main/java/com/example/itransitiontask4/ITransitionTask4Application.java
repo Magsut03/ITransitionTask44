f0e2b7d5399f929e4fea07d4de34fb49522128c4
Magsut <maqsutyangiboyev@gmail.com>
2022-06-04 08:38:57 +0500

    init

---
+++ src/main/java/com/example/itransitiontask4/ITransitionTask4Application.java
@@ -0,0 +1,13 @@
+package com.example.itransitiontask4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITransitionTask4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ITransitionTask4Application.class, args);
+    }
+
+}
